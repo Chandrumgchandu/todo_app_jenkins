bbe64847ca2404537d177824d7049df01a03a39d
Chandru M G <chandrumg124@gmail.com>
2026-06-05 10:53:26 +0000

    jenkins added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from DevOps Project!");
+  res.send("Hello from DevOps Project!; Jenikin done");
 });
 
 app.get("/health", (req, res) => {
