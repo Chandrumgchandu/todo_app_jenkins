b8a0b681531e5c8f25f8bcedb780d707d712e876
Chandru M G <chandrumg124@gmail.com>
2026-06-06 07:29:23 +0000

    Fix loadTodos loop config added..

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,10 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const jwt = require("jsonwebtoken");
-
-const JWT_SECRET = process.env.JWT_SECRET;
-
 function auth(req, res, next) {
 
     const header =
