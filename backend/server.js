501a8f088b55d8e55f5eaf9a3d5e37ab91b0ed29
Chandru M G <chandrumg124@gmail.com>
2026-06-06 00:29:50 +0000

    Rename frondend to frontend and serve frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/api/message", (req, res) => {
     res.json({
