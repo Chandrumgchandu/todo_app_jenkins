eec0bd269ac7968744d8f0980f2e0767451028de
Ubuntu <ubuntu@ip-172-31-19-11.ap-southeast-2.compute.internal>
2026-06-06 01:53:29 +0000

    v1 full stack with .js update

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const pool = require("./db");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.get("/api/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/api/message", (req, res) => {
+    res.json({
+        message: "Hello from DevOps Project v2!"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
